5b5f4375274c94bfb201fab33edca2921976fee0
Gerardo Melendrez <payini@hotmail.com>
2022-07-17 02:15:24 -0700

    Add IdentityManagerBlazorServer demo app.

---
+++ IdentityManagerBlazorServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using IdentityManager;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManagerBlazorServer.Data
+{
+    // Change public class ApplicationDbContext : IdentityDbContext to bottom
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //add OnModelCreating
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .HasMany(p => p.Roles).WithOne()
+                .HasForeignKey(p => p.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ApplicationUser>()
+                .HasMany(e => e.Claims)
+                .WithOne().HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ApplicationRole>()
+                .HasMany(r => r.Claims).WithOne()
+                .HasForeignKey(r => r.RoleId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ IdentityManagerBlazorServer/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManagerBlazorServer.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
